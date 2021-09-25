4afffb75ec05115e7d0525a454cd79cce7ab6545
cjen1 <cjj39@cam.ac.uk>
2021-09-25 17:26:03 +0100

    update nix dependencies

--- shell.nix
+++ shell.nix
@@ -1,8 +1,21 @@
 { pkgs ? import <nixpkgs> {} }:
 
+let 
+  mach-nix = import (builtins.fetchGit {
+    url = "https://github.com/DavHau/mach-nix/";
+    ref = "3.3.0";
+  }) {};
+  mach-python = mach-nix.mkPython {
+    requirements = ''
+      solidpython
+      numpy
+      scipy
+      setuptools
+    '';
+  };
+in
 pkgs.mkShell {
-  buildInputs = [
-    pkgs.cmake
-    pkgs.cq-editor
+  buildInputs = with pkgs; [
+    mach-python
   ];
 }
